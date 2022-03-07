ae8c20342ca535eb62ec6492a958a993a6288e5f
ics-ikeda <ikeda@ics-web.jp>
2022-03-07 23:22:41 +0900

    プロキシーを設定

--- cc-front-react/vite.config.ts
+++ cc-front-react/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      '/read': 'http://localhost:8080',
+      '/add': 'http://localhost:8080',
+      '/remove': 'http://localhost:8080',
+      '/update': 'http://localhost:8080',
+    }
+  }
 })
